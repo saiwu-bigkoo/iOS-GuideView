81184072dcffef9a89743710e0cd573bc433947b
sai <sai.wu@bigkoo.com>
2014-11-25 19:10:36 +0800

    UIGuide
    iOS guide view with swift  // swift 写的引导界面

---
+++ GuideView/GuideView.swift
@@ -0,0 +1,22 @@
+//
+//  GuideView.swift
+//  GuideView
+//
+//  Created by StephenZhu on 24/11/14.
+//  Copyright (c) 2014年 Sai. All rights reserved.
+//
+
+import UIKit
+
+class GuideView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
